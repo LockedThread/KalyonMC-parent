e50e7d96fa9af3c9ce48e5053f5da4268f3f9626
LockedThread <proteinshakedev@gmail.com>
2019-08-31 10:33:20 -0400

    Optimized and fixed anvil check and fixed the enchantment table check

--- DisableUtility/src/main/java/dev/lockedthread/kalyonmc/disableutility/listeners/BlockedEnchantListener.java
+++ DisableUtility/src/main/java/dev/lockedthread/kalyonmc/disableutility/listeners/BlockedEnchantListener.java
@@ -8,27 +8,23 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.enchantment.EnchantItemEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.inventory.AnvilInventory;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-
-import java.util.Map;
 
 public class BlockedEnchantListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onEnchantItemEvent(EnchantItemEvent event) {
-        for (Map.Entry<Enchantment, Integer> entry : event.getEnchantsToAdd().entrySet()) {
-            if (entry.getKey().getName().equalsIgnoreCase("knockback") || entry.getKey().getName().equalsIgnoreCase("punch")) {
-                event.getEnchantsToAdd().remove(entry.getKey());
-            }
+        if (event.getEnchantsToAdd().containsKey(Enchantment.ARROW_KNOCKBACK) || event.getEnchantsToAdd().containsKey(Enchantment.KNOCKBACK)) {
+            event.setCancelled(true);
         }
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onInventoryClick(InventoryClickEvent event) {
-        if (event.getClickedInventory() != null && event.getClickedInventory().getHolder() instanceof AnvilInventory && event.getWhoClicked() instanceof Player) {
-            Inventory inventory = event.getView().getTopInventory();
+        Inventory inventory = event.getView().getTopInventory();
+        if (inventory != null && event.getClickedInventory() != null && inventory.getType() == InventoryType.ANVIL && event.getWhoClicked() instanceof Player) {
             ItemStack firstItem = inventory.getItem(0);
             ItemStack secondItem = inventory.getItem(1);
             if (firstItem != null && secondItem != null && firstItem.getType() != Material.AIR && secondItem.getType() != Material.AIR) {
